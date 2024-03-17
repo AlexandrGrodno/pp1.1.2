c9d712811fe02b8b193954542213613bb180b59f
AlexandrGrodno <avgust_sa@mail.ru>
2024-03-17 23:51:31 +0300

    fix

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,6 +2,8 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.sql.SQLException;
 
@@ -9,16 +11,16 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
 
-        UserDao userDao = new UserDaoJDBCImpl();
+        UserService userService = new UserServiceImpl();
 
-        userDao.createUsersTable();
-        userDao.saveUser("user1","name1", (byte) 1);
-        userDao.saveUser("user2","name2", (byte) 2);
-        userDao.saveUser("user3","name3", (byte) 3);
-        userDao.saveUser("user4","name4", (byte) 4);
-        userDao.getAllUsers();
-        userDao.cleanUsersTable();
-        userDao.dropUsersTable();
+        userService.createUsersTable();
+        userService.saveUser("user1","name1", (byte) 1);
+        userService.saveUser("user2","name2", (byte) 2);
+        userService.saveUser("user3","name3", (byte) 3);
+        userService.saveUser("user4","name4", (byte) 4);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
 //       }
     }
 }
